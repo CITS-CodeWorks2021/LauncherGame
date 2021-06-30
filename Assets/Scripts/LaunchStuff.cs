4a95eeea51f4c2c756863fd9969ebaec60bec7bc
JBF <john@codeintheschools.org>
2021-06-30 12:38:19 -0400

    some stuff

--- Assets/Scripts/LaunchStuff.cs
+++ Assets/Scripts/LaunchStuff.cs
@@ -12,27 +12,58 @@ public class LaunchStuff : MonoBehaviour
 
     public int ranNumber;
 
+    Rigidbody2D blah;
+
+    Vector2 wat;
+
+
     GameObject spawnedBit;
+
+    float myTimer = 0;
+    float endTime = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        spawnedBit = Instantiate(stuff);
-        
+        //spawnedBit = Instantiate(stuff);
+
+        stuff.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0; i < 3; i++)
+        //for(int i = 0; i < themThings.Length; i++)
+        //{
+        //    if (themThings[i] != null) themThings[i].transform.Translate(0, 0.1f, 0);
+        //}
+
+        if(myTimer > endTime)
         {
-            if (spawnedBit != null) spawnedBit.transform.Translate(0, 0.1f, 0);
+            // My delayed code
+            Debug.LogWarning("Uh Oh! No Timed code here!");
+            myTimer = 0;
+        }
+        else
+        {
+            myTimer += Time.deltaTime;
         }
         
     }
 
-    public void LaunchSomething()
+    public GameObject LaunchSomething()
     {
         // Here We Launch
+        Debug.Log("Yup, You done Launched!");
+        return stuff;
+    }
+
+    public int AddOne(int fromWhat)
+    {
+        int mReturn = fromWhat + 1;
+
+        return mReturn;
     }
 }
